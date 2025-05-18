f20f354d91aedda1d8f29928bab29df4b7d4a066
Your Name <your_email>
2025-05-18 14:35:36 +0300

    fix #4

--- js/task-4.js
+++ js/task-4.js
@@ -1,15 +1,15 @@
 document.addEventListener("submit", submitHandleFunc);
 function submitHandleFunc(sbEv) {
   sbEv.preventDefault();
-  // console.log(sbEv.target);
-
+  const fmIn = sbEv.target;
+  // console.log(fmIn[0].value);
   // console.log(`object "${sbEv.target[0].name}" && "${sbEv.target[1].name}"`);
-  if (sbEv.target[0].value && sbEv.target[1].value) {
-    const login = {
-      [sbEv.target[0].name]: sbEv.target[0].value.trim(),
-      [sbEv.target[1].name]: sbEv.target[1].value.trim(),
-    };
-    console.log(login);
+  if (fmIn[0].value.trim() && fmIn[1].value.trim()) {
+    // const login =
+    console.log({
+      [fmIn[0].name]: fmIn[0].value.trim(),
+      [fmIn[1].name]: fmIn[1].value.trim(),
+    });
     sbEv.target.reset();
   } else {
     alert("All form fields must be filled in");
